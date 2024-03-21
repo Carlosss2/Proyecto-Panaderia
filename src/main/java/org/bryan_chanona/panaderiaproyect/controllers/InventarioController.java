268d99937214cad4b7d71cd9b3a0bd8c9852cd86
jared torres <233310@ids.upchiapas.edu.mx>
2024-03-21 04:15:48 -0600

    Interfaz InventarioProducto

---
+++ src/main/java/org/bryan_chanona/panaderiaproyect/controllers/InventarioController.java
@@ -0,0 +1,20 @@
+package org.bryan_chanona.panaderiaproyect.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class InventarioController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
