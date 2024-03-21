7af426bfe56895d30b222e937104022e875a8d72
BryanChanona <233299@ids.upchiapas.edu.mx>
2024-03-21 02:54:29 -0600

    Agregar vista para productos

--- src/main/java/org/bryan_chanona/panaderiaproyect/controllers/ProductoController.java
+++ src/main/java/org/bryan_chanona/panaderiaproyect/controllers/ProductoController.java
@@ -1,4 +1,41 @@
 package org.bryan_chanona.panaderiaproyect.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
 public class ProductoController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void onMouseClickAgregarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onMouseClickModificarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onMouseClickVenderButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onMouseClickeliminarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
