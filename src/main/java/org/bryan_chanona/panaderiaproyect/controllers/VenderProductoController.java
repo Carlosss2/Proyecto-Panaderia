b79bcccdeaa88635a46abce0d6f864a838b6de43
jared torres <233310@ids.upchiapas.edu.mx>
2024-03-21 03:39:13 -0600

    interfaz de agregar productos

--- src/main/java/org/bryan_chanona/panaderiaproyect/controllers/VenderProductoController.java
+++ src/main/java/org/bryan_chanona/panaderiaproyect/controllers/VenderProductoController.java
@@ -1,4 +1,25 @@
 package org.bryan_chanona.panaderiaproyect.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class VenderProductoController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button YEMA;
+
+    @FXML
+    void initialize() {
+        assert YEMA != null : "fx:id=\"YEMA\" was not injected: check your FXML file 'venderProducto-view.fxml'.";
+
+    }
+
 }
