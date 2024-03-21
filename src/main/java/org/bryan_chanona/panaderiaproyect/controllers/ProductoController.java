f1e1d0d31af8b3537f5730f8fde4faa8096dd9f5
BryanChanona <233299@ids.upchiapas.edu.mx>
2024-03-21 03:40:22 -0600

    Agregando interfaz para agregar producto

--- src/main/java/org/bryan_chanona/panaderiaproyect/controllers/ProductoController.java
+++ src/main/java/org/bryan_chanona/panaderiaproyect/controllers/ProductoController.java
@@ -1,10 +1,14 @@
 package org.bryan_chanona.panaderiaproyect.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import org.bryan_chanona.panaderiaproyect.App;
 
 public class ProductoController {
 
@@ -17,10 +21,15 @@ public class ProductoController {
     Stage callModificarproducto = new Stage();
     Stage callVenderProducto =  new Stage();
     Stage callEliminarProducto = new Stage();
-    
+
 
     @FXML
-    void onMouseClickAgregarButton(MouseEvent event) {
+    void onMouseClickAgregarButton(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("agregarProducto-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        callAgregarproduct.setTitle("Hello!");
+        callAgregarproduct.setScene(scene);
+        callAgregarproduct.show();
 
 
     }
