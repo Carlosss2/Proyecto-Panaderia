a9282e4be2108b4f36f2fd9039b81e35742a84f6
jared torres <233310@ids.upchiapas.edu.mx>
2024-03-21 03:55:16 -0600

    Interfaz EliminarProducto

---
+++ src/main/java/org/bryan_chanona/panaderiaproyect/controllers/EliminarController.java
@@ -0,0 +1,20 @@
+package org.bryan_chanona.panaderiaproyect.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class EliminarController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
