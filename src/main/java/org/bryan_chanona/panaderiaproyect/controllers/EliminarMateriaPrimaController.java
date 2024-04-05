8527c5a03cf8de498209e606cea3256dcea1af43
Carlosss2 <233286@ids.upchiapas.edu.mx>
2024-04-04 23:38:28 -0600

    Implementacion de logica para eliminar materiaPrimas

--- src/main/java/org/bryan_chanona/panaderiaproyect/controllers/EliminarMateriaPrimaController.java
+++ src/main/java/org/bryan_chanona/panaderiaproyect/controllers/EliminarMateriaPrimaController.java
@@ -1,37 +1,67 @@
 package org.bryan_chanona.panaderiaproyect.controllers;
+
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import org.bryan_chanona.panaderiaproyect.App;
+import org.bryan_chanona.panaderiaproyect.models.Panaderia;
+import org.bryan_chanona.panaderiaproyect.models.Producto;
 
 public class EliminarMateriaPrimaController {
 
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField cantidadAeliminar;
+
+    @FXML
+    private ComboBox<String> tipoMateriaComboBox;
+
+    @FXML
+    void bottonEliminar(MouseEvent event) {
+        Panaderia listaPanes = App.getPan();
+        Integer cantidad = Integer.parseInt(cantidadAeliminar.getText());
+        if (!listaPanes.getMaterial().isEmpty()) {
+
+            List<Producto> productosAEliminar = new ArrayList<>();
+
+            for (Producto producto : listaPanes.getMaterial()) {
+                if (tipoMateriaComboBox.getValue().equals(producto.getNombrePan()) && cantidad > 0) {
+                    if (producto.getCantidadProducto() >= cantidad) {
+                        producto.setCantidadProducto(producto.getCantidadProducto() - cantidad);
 
 
-
-
-
-        @FXML
-        private ResourceBundle resources;
-
-        @FXML
-        private URL location;
-
-        @FXML
-        private Button YEMA;
-
-        @FXML
-        void initialize() {
-
-
+                        if (producto.getCantidadProducto() == 0) {
+                            productosAEliminar.add(producto);
+                        }
+                    }
+                }
+            }
+            
+            listaPanes.getPanes().removeAll(productosAEliminar);
         }
 
-    public void bottonSalir(MouseEvent mouseEvent) {
+
     }
 
-        public void bottonEliminar(MouseEvent mouseEvent) {
-        }
+    @FXML
+    void bottonSalir(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        tipoMateriaComboBox.getItems().addAll("Harina","Levadura","Azucar","Mantequilla","Vainilla","Ajonjoli","Leche");
+
+    }
+
 }
-
-
